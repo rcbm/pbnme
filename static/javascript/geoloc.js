88960e1112b5d16af34780e26eb03b8090ebdc59
mattbarackman <matthew.barackman@gmail.com>
2011-11-05 21:48:02 -0700

    added city finder

--- static/javascript/geoloc.js
+++ static/javascript/geoloc.js
@@ -2,71 +2,72 @@ jQuery(window).ready(function(){
     jQuery("#btnInit").click(initiate_geolocation);
 })
 
-function initiate_geolocation() {
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.getCurrentPosition(handle_geolocation_query, handle_errors);
-    }
-    else
-    {
-        yqlgeo.get('visitor', normalize_yql_response);
-    }
-}
-
-function handle_errors(error)
-{
-    switch(error.code)
-    {
-        case error.PERMISSION_DENIED: alert("user did not share geolocation data");
-        break;
-
-        case error.POSITION_UNAVAILABLE: alert("could not detect current position");
-        break;
-
-        case error.TIMEOUT: alert("retrieving position timedout");
-        break;
-
-        default: alert("unknown error");
-        break;
-    }
-}
-
-function normalize_yql_response(response)
-{
-    if (response.error)
-    {
-        var error = { code : 0 };
-        handle_error(error);
-        return;
-    }
-
-    var position = {
-        coords :
+    function initiate_geolocation() {
+        if (navigator.geolocation)
         {
-            latitude: response.place.centroid.latitude,
-            longitude: response.place.centroid.longitude
-        },
-        address :
-        {
-            city: response.place.locality2.content,
-            region: response.place.admin1.content,
-            country: response.place.country.content
+            navigator.geolocation.getCurrentPosition(handle_geolocation_query, handle_errors);
         }
-    };
+        else
+        {
+            yqlgeo.get('visitor', normalize_yql_response); //change to Roger's new thing
+        }
+    }
 
-    handle_geolocation_query(position);
-}
+    function handle_errors(error)
+    {
+        switch(error.code)
+        {
+            case error.PERMISSION_DENIED: alert("user did not share geolocation data");
+            break;
+
+            case error.POSITION_UNAVAILABLE: alert("could not detect current position");
+            break;
+
+            case error.TIMEOUT: alert("retrieving position timedout");
+            break;
+
+            default: alert("unknown error");
+            break;
+        }
+    }
+
+    function normalize_yql_response(response)  
+    {  
+        if (response.error)  
+        {  
+            var error = { code : 0 };  
+            handle_error(error);  
+            return;  
+        }  
+  
+        var position = {  
+            coords :  
+            {  
+                latitude: response.place.centroid.latitude,  
+                longitude: response.place.centroid.longitude  
+            },  
+            address :  
+            {  
+                city: response.place.locality2.content,  
+                region: response.place.admin1.content,  
+                country: response.place.country.content  
+            }  
+        };  
+  
+        handle_geolocation_query(position);  
+    }
 
 	function handle_geolocation_query(position)
 	{
-	    var image_url = "http://maps.google.com/maps/api/staticmap?sensor=false&center=" + position.coords.latitude + "," +
-	                    position.coords.longitude + "&zoom=14&size=300x400&markers=color:blue|label:S|" +
-	                    position.coords.latitude + ',' + position.coords.longitude;
+        $("#lat").html(position.coords.latitude);
+        $("#long").html(position.coords.longitude);
 
-	    jQuery("#map").remove();
-	    jQuery(document.body).append(
-	        jQuery(document.createElement("img")).attr("src", image_url).attr('id','map')
-	        );
-        $('#lat').html=position.coords.latitude;
-        $('#long').html=position.coords.longitude;
+       if ([Math.abs([position.coords.latitude - 37.871667]) < .27] && [Math.abs([position.coords.longitude - -122.272778]) < .27])
+         {alert("berkeley")}
+       else if ([Math.abs([position.coords.latitude - 440.441667]) < .27] && [Math.abs([position.coords.longitude - -80]) < .27])
+         {alert("pittsburgh")}
+       else
+         {alert("too far away")}
 	}
+
+    //+-0.27 degrees  Pittsburgh: 440.441667, -80,  Berkeley: 37.871667, -122.272778
